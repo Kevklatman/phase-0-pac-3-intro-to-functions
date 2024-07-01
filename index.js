f96d7231fc83127f9a78768c9dd5a033d4e9a328
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-06-30 20:23:34 -0600

    lab completion

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+sayHello();
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function doSomething(thing) {
+    console.log(thing);
+  }
+  doSomething("anything"); 
+
+function printName (name) {
+    console.log(name)
+} 
+    printName ("Kevin");
+
+function eatHamburgers(lettuce) {
+    console.log(lettuce)
+}
+    eatHamburgers("Peyton Manning")
+
+function sayHelloTo(firstName) {
+    console.log(`Hello there, ${firstName}`)
+}   sayHelloTo("Guadalupe");
+    sayHelloTo("Jane");
+    sayHelloTo("R2-D2")
+    sayHelloTo(1)
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`)
+      }
+    say("Goodbye", "Julio")
+function add(x,y) {
+  return x+y;
+}console.log(add(1,2));
+
+function mult(x,y,z) {
+  return x*y*z;
+}console.log(mult(1,2,3))
+
+function sayHello(greeting, name){
+console.log(`Hey, ${greeting}, ${name}`)
+} sayHello("Hello", "Henry")
+
